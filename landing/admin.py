1838f67431f78052f3eb92bd58abc8fa970b06ae
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-01-30 02:52:06 +0300

    Группировка полей в админке

--- landing/admin.py
+++ landing/admin.py
@@ -3,8 +3,33 @@ from .models import Landing
 
 
 class LandingAdmin(admin.ModelAdmin):
-    list_display = ['header_title_1', 'header_content', 'is_published', 'django_content']
+    list_display = ['header_title_1', 'is_published']
     list_editable = ['is_published']
+    fieldsets = (
+        ('Настройки Header(первый экран) - заголовки', {
+            'fields': ('header_title_1', 'header_title_2', 'header_content')
+        }),
+        ('Настройки Inspire (второй экран) - заголовки', {
+            'fields': ('inspire_title_1', 'inspire_title_2')
+        }),
+        ('Настройки Inspire (второй экран) - заполнение контента для секции Django', {
+            'fields': ('django_content', 'django_img')
+        }),
+        ('Настройки Inspire (второй экран) - заполнение контента для секции Bootstrap', {
+            'fields': ('bootstrap_content', 'bootstrap_img')
+        }),
+        ('Настройки Inspire (второй экран) - заполнение контента для секции CSS', {
+            'fields': ('css_content', 'css_img')
+        }),
+        ('Настройки attainment (низ второго экрана) - заполнение контента для Django(левый блок)', {
+            'fields': ('attainment_django_num', 'attainment_django_title', 'attainment_django_content')
+        }),
+        ('Настройки attainment (низ второго экрана) - заполнение контента для Bootstrap (центральный блок)', {
+            'fields': ('attainment_bootstrap_num', 'attainment_bootstrap_title', 'attainment_bootstrap_content')
+        }),
+        ('Настройки attainment (низ второго экрана) - заполнение контента для CSS (правый блок)', {
+            'fields': ('attainment_css_num', 'attainment_css_title', 'attainment_css_content')
+        }),
 
-
+    )
 admin.site.register(Landing, LandingAdmin)

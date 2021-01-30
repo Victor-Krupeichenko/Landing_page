d2398637f0d6a04dd5c6ed211c7a1f0b117d6e6e
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-01-30 14:41:22 +0300

    Уменьшен размер полей формы для текстового ввода

--- landing/admin.py
+++ landing/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from .models import Landing
+from django.forms import TextInput, Textarea
+from django.db import models
 
 
 class LandingAdmin(admin.ModelAdmin):
     list_display = ['header_title_1', 'is_published']
     list_editable = ['is_published']
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': '50'})},
+        models.TextField: {'widget': Textarea(attrs={'rows': 5, 'cols': 80})},
+    }
     fieldsets = (
         ('Настройки Header(первый экран) - заголовки', {
             'fields': ('header_title_1', 'header_title_2', 'header_content')
@@ -31,7 +37,7 @@ class LandingAdmin(admin.ModelAdmin):
             'fields': ('attainment_css_num', 'attainment_css_title', 'attainment_css_content')
         }),
         ('Опубликовать', {
-            'fields': ('is_published',),
+            'fields': ('is_published', ),
             'description': 'Чтобы опубликовать второй вариант заполнения - НЕОБХОДИМО СНЯТЬ ОПУБЛИКОВАНО С '
                            'ПРЕДЫДУЩЕГО ВАРИАНТА',
             'classes': ['collapse']

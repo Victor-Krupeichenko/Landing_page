1fe60abd80e22fbc0a132cedc529ed2094b20344
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-01-31 15:59:45 +0300

    немного поправлены поля

--- landing/models.py
+++ landing/models.py
@@ -28,10 +28,22 @@ class Landing(models.Model):
     attainment_css_title = models.CharField(max_length=31, verbose_name='Заголовок')
     attainment_css_content = models.TextField(max_length=111, verbose_name='Контент')
     is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
-    lets_title_1 = models.CharField(max_length=18, verbose_name='Певый заголовок', null=True)
-    lets_title_2 = models.CharField(max_length=42, verbose_name='Второй заголовок', blank=True, null=True,
-                                    help_text='Это поля необязательно к заполнению')
-    lets_content = models.TextField(max_length=171, verbose_name='Контент', null=True)
+    lets_title_1 = models.CharField(max_length=18, verbose_name='Певый заголовок')
+    lets_title_2 = models.CharField(max_length=42, verbose_name='Второй заголовок', blank=True,
+                                    help_text='Это поле необязательно к заполнению')
+    lets_content = models.TextField(max_length=171, verbose_name='Контент')
+    crm_title_1 = models.CharField(max_length=32, verbose_name='Первый заголовок')
+    crm_title_2 = models.CharField(max_length=43, verbose_name='Второй заголовок', blank=True,
+                                   help_text='Это поле необязательно к заполнению')
+    crm_content = models.TextField(max_length=221, verbose_name='Контент')
+    crm_section_1 = models.CharField(max_length=14, verbose_name='Заголовок')
+    crm_section_content = models.TextField(max_length=110, verbose_name='Контент')
+    crm_section_2 = models.CharField(max_length=14, verbose_name='Заголовок')
+    crm_section_2_content = models.TextField(max_length=110, verbose_name='Контент')
+    crm_video_1 = models.TextField(verbose_name='Видео', blank=True,
+                                   help_text='Это поле необязательно к заполнению')
+    crm_video_2 = models.TextField(verbose_name='Видео', blank=True,
+                                   help_text='Это поле необязательно к заполнению')
 
     # def __str__(self):
     #     return self.header_title_1
